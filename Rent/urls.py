08932420a1762c1d794faf8a4881fa19b712ee69
himanshu311201 <himanshu312017@gmail.com>
2021-10-09 16:44:35 +0530

    Change

--- Rent/urls.py
+++ Rent/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('',views.index,name="home"),
     path('product/',views.Productform,name="product"),
     path('ajax/load_subcat/', views.load_subcat, name='ajax_load_subcat'),
+    path('wedding/',views.Weddings,name="product"),
+    path('subcategory/<str:mysubcat>',views.search_subcat,name="Search subcat"),
 ]
