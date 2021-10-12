c142677c231b06e7637f3f722b4e5ffe596f08c0
Pushkar-PM <pushkarmavale@gmail.com>
2021-10-12 13:58:55 +0530

    product view page

--- Rent/views.py
+++ Rent/views.py
@@ -2,9 +2,11 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.views.generic import CreateView
-from .models import Product,Photo,Subcategory,Category
+from .models import Product,Photo,Subcategory,Category,Photo
 from User.models import Seller
-from .forms import ProductForm
+from .forms import ProductForm,PhotoForm
+from django.urls import reverse,resolve
+from urllib.parse import urlencode
 # Create your views here.
 def index(request):
     products=Product.objects.all()
@@ -15,10 +17,12 @@ def index(request):
     }
     print(products)
     return render(request, 'Rent/home_page.html',param)
+
 def search_subcat(request,mysubcat):
     category = get_object_or_404(Subcategory, subcategories=mysubcat)
     products = Product.objects.filter(subcategory=category)
     return render(request, 'Rent/subcstfil.html',{'product':products})
+
 def Weddings(request):
     subcat=['Ethnic','Drum','Gifts','Car']
     param={
@@ -26,14 +30,17 @@ def Weddings(request):
     }
     print(subcat)
     return render(request,'Rent/Weddings.html',param)
+
 def load_subcat(request):
     print('Hello')
     category_id = request.GET.get('CategoryId')
     subcategory = Subcategory.objects.filter(Categories=category_id).all()
     print(subcategory)
     return render(request, 'Rent/subcat.html', {'subcat': subcategory})
+
 def show(request):
     return render(request, 'Rent/booking.html')
+
 def Productform(request):
     if request.method=='POST' :
         form=ProductForm(request.POST,request.FILES)
@@ -44,12 +51,61 @@ def Productform(request):
             p = form.save(commit=False)
             p.seller_of_item = u
             p.availability=True
+            p_img=p.image_of_product
+            print(p_img)
             p.save()
-            products = Product.objects.filter(id=(p.pk))
-            return render(request, 'Rent/ProdView.html', {'Product': products})
+            products = Product.objects.get(id=(p.pk))
+            ph=Photo(photo=p_img,product_photo=products)
+            request.session['p_id']=p.pk
+            ph.save()
+            # base=reverse('Prod_view')
+            # query=urlencode({'prod_id':p.pk})
+            # url='{}?{}'.format(base,query)
+            return redirect('Prod_view',p.pk)
         else:
             print(form.errors)
             return render(request,'Rent/Product.html',{'form':form})
     else:
         form=ProductForm()
     return render(request,'Rent/Product.html',{'form':ProductForm})
+
+
+def Prod_view(request,prod_id):
+    # print(pk)
+    ppk=prod_id
+    context={
+        'Product':Photo.objects.filter(product_photo__id=ppk),
+        'pro':Product.objects.get(id=ppk),
+        'pform':PhotoForm,
+    }
+    return render(request,'Rent/ProdView.html',context)
+
+
+def load(request):
+    ppk=request.session['p_id']
+    context={
+        'Product':Photo.objects.filter(product_photo__id=ppk),
+        'pro':Product.objects.get(id=ppk),
+        'pform':PhotoForm,
+    }
+    return render(request,'Rent/loop.html',context)
+
+
+def AddImage(request):
+    if request.method=='POST':
+        current_url = resolve(request.path_info).url_name
+        print(request.POST)
+        print(request.get_full_path())
+        # if 'djj' in request.POST:
+        #     print("KDSKSk")
+        t=request.POST.get('djj')
+        # # t1=request.FILES['djj']
+        form=PhotoForm(t)
+        # print(t)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({'bool':True})
+        else:
+            print(form.errors)
+
+    return JsonResponse({'bool':True})
