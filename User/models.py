b9b9f8215ec5c374c3c098444352aff85ba0b1fc
Pushkar-PM <pushkarmavale@gmail.com>
2021-11-12 02:01:58 +0530

    Date field added

--- User/models.py
+++ User/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MaxLengthValidator,MinLengthValidator
 # Create your models here.
 class Profile(models.Model):
 
-    user=models.OneToOneField(User, on_delete=models.CASCADE)
+    user=models.OneToOneField(User,related_name='profile', on_delete=models.CASCADE)
     bank_acc_no=models.CharField(max_length=16,validators=[MinLengthValidator(16)])
     ifsc=models.CharField(max_length=11,validators=[MinLengthValidator(11)])
     address=models.TextField()
