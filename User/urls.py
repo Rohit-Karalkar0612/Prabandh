b3269af491f9dd37a4ea74c0d91665c5d9b14b7f
Pushkar-PM <pushkarmavale@gmail.com>
2021-11-12 17:15:01 +0530

    sda

--- User/urls.py
+++ User/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('',user_views.Register,name="register"),
     path('profile/',user_views.profile,name="profile"),
     path('seller/',user_views.SellerView,name="seller"),
-    path('login/',auth_views.LoginView.as_view(template_name='User\login.html'),name='login'),
-    path('logout/',auth_views.LogoutView.as_view(template_name='Rent\home_page.html'),name='logout'),
+    path('login/',auth_views.LoginView.as_view(template_name='User/login.html'),name='login'),
+    path('logout/',auth_views.LogoutView.as_view(template_name='Rent/home_page.html'),name='logout'),
 ]
