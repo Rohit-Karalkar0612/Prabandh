47717fabeef6789807a931d7aaf55a1c65eec519
Pushkar-PM <pushkarmavale@gmail.com>
2022-04-15 14:14:14 +0530

    pwa

--- Prabandh/settings.py
+++ Prabandh/settings.py
@@ -156,8 +156,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 PWA_APP_NAME = 'Prabandh'
 PWA_APP_DESCRIPTION = "My app description"
-PWA_APP_THEME_COLOR = '#0A0302'
-PWA_APP_BACKGROUND_COLOR = '#ffffff'
+PWA_APP_THEME_COLOR = '#0610c9'
+PWA_APP_BACKGROUND_COLOR = '#0610c9'
 PWA_APP_DISPLAY = 'standalone'
 PWA_APP_SCOPE = '/'
 PWA_APP_ORIENTATION = 'any'
@@ -165,7 +165,7 @@ PWA_APP_START_URL = '/'
 PWA_APP_STATUS_BAR_COLOR = 'default'
 PWA_APP_ICONS = [{'src': '/static/images/my_app_icon.png', 'sizes': '160x160'}]
 PWA_APP_ICONS_APPLE = [
-    {'src': '/static/images/my_apple_icon.png', 'sizes': '160x160'}]
+    {'src': '/static/images/my_app_icon.png', 'sizes': '160x160'}]
 PWA_APP_SPLASH_SCREEN = [{'src': '/static/images/icons/splash-640x1136.png',
                           'media': '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)'}]
 PWA_APP_DIR = 'ltr'
